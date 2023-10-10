835dead50482adac9d9cbd6dd622792ec80f6fbb
amirasaeed32 <amirasaeed637@gmail.com>
2023-10-10 05:09:49 -0700

    functions

--- function.c
+++ function.c
@@ -384,22 +384,3 @@ char *convert_number(long int num, int base, int flags)
 		*--p = sign;
 	return (p);
 }
-/**
- * print-error - printa an error message
- * @info: the parameter and return info struct
- * @estr: string containig specified error type
- * Return: 0 if no numbers in string, converted number otherwise
- * -1 on error
- */
-void print_error(info_t *info, char *estr)
-{
-	_eputs(info->fname);
-	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_eputs(": ");
-	_eputs(info->argv[0]);
-	_eputs(": ");
-	_eputs(estr);
-}
-
-
