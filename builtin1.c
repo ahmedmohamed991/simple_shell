7407e966cddf776d8272636deaa73073facdb0bb
ahmedmohamed991 <ahmmeedhafez22@gmail.com>
2023-10-11 00:11:12 +0300

    updated

--- builtin1.c
+++ builtin1.c
@@ -6,8 +6,8 @@
 */
 int _myhistory(info_t *info)
 {
-    print_list(info->history);
-    return (0);
+	print_list(info->history);
+	return (0);
 }
 
 /**
@@ -17,33 +17,37 @@ int _myhistory(info_t *info)
 *
 *Return: always 0 on success, 1 on error
 */
-int unset_alias(info_t *info,char *str)
+int unset_alias(info_t *info, char *str)
 {
-    int x;
-    char *pointer, c;
-    pointer = _strchr(str, '=');
-    if(!pointer)
-    return(1);
+	int x;
+	char *pointer, c;
+
+	pointer = _strchr(str, '=');
+
+	if (!pointer)
+		return (1);
 c = *pointer;
 *pointer = 0;
-x = delet_node_at_index(&(info->alias), get_node_index(info->alias, node_starts_with(info, str, -1)));
+x = delet_node_at_index(&(info->alias),
+		get_node_index(info->alias, node_starts_with(info, str, -1)));
 *pointer = c;
 return (x);
 }
 
 /**
-*set_alias - sets alias to string
-*@info: parameter struct
-*@str: the string alias
-*Retur: alwayes 0 on success, 1 on error
+ *
+ *set_alias - sets alias to string
+ *@info: the info struct to set the alias for
+ *@str: the alias string in the form "alias=value"
+ *Retur: alwayes 0 on success, 1 on failure
 */
 int set_alias(info_t *info, char *str)
 {
 char *pointer;
 pointer = _strchr(str, '=');
-if(!pointer)
+if (!pointer)
 return (1);
-if(!*++pointer)
+if (!*++pointer)
 return (unset_alias(info, str));
 unset_alias(info, str);
 return (add_node_end(&(info->alias), str, 0) == NULL);
@@ -57,18 +61,20 @@ return (add_node_end(&(info->alias), str, 0) == NULL);
 */
 int print_alias(list_t *node)
 {
-    char *x = NULL, *pointer = NULL;
-    if(node)
-    {
-        pointer = _strchr(node->str, '=');
-        for (x = node->str;a <= pointer; x++)
-        _putchar(*x);
-        _putchar(" \ ");
-        _puts(pointer + 1);
-        _puts("\n");
-        return (0);
-    }
-    return (1);
+	char *x = NULL, *pointer = NULL;
+
+	if (node)
+	{
+	    pointer = _strchr(node->str, '=');
+
+		for (x = node->str; a <= pointer; x++)
+			_putchar(*x);
+	    _putchar(" \ ");
+	    _puts(pointer + 1);
+	    _puts("\n");
+		return (0);
+	}
+	return (1);
 }
 
 /**
@@ -78,26 +84,28 @@ int print_alias(list_t *node)
 */
 int _myalias(info_t *info)
 {
-    char *pointer = NULL;
-    int x = 0;
-    list_t *node = NULL;
-    if (info->argc == 1)
-    {
-        node = info->alias;
-        while (node)
-        {
-            print_alias(node);
-            node = node->next;
-        }
-        return (0);
-    }
-    for (x = 1; info->argv[x]; x++)
-    {
-        pointer = _strchr(info->argv[x], '=')
-        if (pointer)
-        set_alias(info, info->argv[x]);
-    else
-    print_alias(node_starts_with(info->alias, info->argv[x], '='));
-    }
-    return (0);
+	char *pointer = NULL;
+	int x = 0;
+	list_t *node = NULL;
+
+	if (info->argc == 1)
+	{
+		node = info->alias;
+
+		while (node)
+		{
+		print_alias(node);
+		node = node->next;
+		}
+		return (0);
+	}
+	for (x = 1; info->argv[x]; x++)
+	{
+	    pointer = _strchr(info->argv[x], '=')
+		if (pointer)
+		set_alias(info, info->argv[x]);
+		else
+			print_alias(node_starts_with(info->alias, info->argv[x], '='));
+	}
+	return (0);
 }
---
+++ function2.c
@@ -0,0 +1,144 @@
+#include "shell.h"
+/**
+*delet_node_at_index - delets node at given index
+*@head: address of pointer to first node
+*@index: index of node to delet
+*Return: 1 on success, 0 on failure
+*/
+int delet_node_at_index(list_t **head, unsigned int index)
+{
+    list_t *node, *prev_node;
+    unsigned int x = 0;
+    if (!head || !*head)
+    return (0);
+if (!index)
+{
+    node = *head;
+    *head = (*head)->next;
+    free(node->str);
+    return (1);
+}
+node = *head;
+while (node)
+{
+    if (x == index)
+    {
+        prev_node->next = node->next;
+        free(node->str);
+        free(node);
+        return (1);
+    }
+    x++;
+    prev_node = node;
+    node = node->next;
+}
+return (0);
+}
+
+/**
+*get_node_index - gets the index of a node
+*@head: pointer to list head
+*@node: pointer to the node
+*Return: index of node or -1
+*/
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+    size_t x = 0;
+
+    while (head)
+    {
+        if (head == node)
+        return (x);
+    head = head->next;
+    x++;
+    }
+    return (-1);
+}
+
+/**
+*_putchar - writes the character c to stdout
+*@c: the character to print
+*Return: on seccess 1.
+*/
+int _putchar(char c)
+{
+    static char buf[WRITE_BUF_SIZE];
+    static int x;
+
+    if (c == BUF_FLUSH || x >= WRITE_BUF_SIZE)
+    {
+        write(1, buf, x);
+        x = 0;
+    }
+    if(c != BUF_FLUSH)
+    buf[x++] = c;
+return (1);
+}
+
+/**
+*_puts - prints an input string
+*@str: the string to be printed
+*Return: nothing
+*/
+void _puts(char *str)
+{
+    int x = 0;
+
+    if (!str)
+    return;
+while (str[x] != '\0')
+{
+    _putchar(str[x]);
+    x++;
+}
+}
+
+/**
+*print_list_str - prints only the str element of a list_t linked list
+*@h: pointer to first node
+*Return: size of list
+*/
+size_t print_list_str(const list_t *h)
+{
+    size_t x = 0;
+
+    while (h)
+    {
+        _puts(h->str ? h->str : "(nil)");
+        _puts("\n");
+        h = h->next;
+        x++;
+    }
+    return (x);
+}
+
+/**
+_unsetenv - remove an environment variable
+@info: strcture containing potential arguments.
+@var: the string env var property
+Return: 1 on delete, 0 otherwise
+*/
+int _unsetenv(info_t *info, char *var)
+{
+    list_t *node = info->env;
+    size_t x = 0;
+    char *pointer;
+
+    if (!node || !var)
+    return (0);
+
+while (node)
+{
+    pointer = starts_with(node->str, var);
+    if (pointer && *pointer == '=')
+    {
+        info->env_changed = delet_node_at_index(&(info->env), x);
+        x = 0;
+        node = info->env;
+        continue;
+    }
+    node = node->next;
+    x++;
+}
+return (info->env_changed);
+}   
